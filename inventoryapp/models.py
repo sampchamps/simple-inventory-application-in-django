605662ef6ab8d379b615bda7206a10a58658c688
sjtg <chipilej@gmail.com>
2023-04-17 18:39:41 +0200

    Added models and modify admin.py

--- inventoryapp/models.py
+++ inventoryapp/models.py
@@ -1,3 +1,12 @@
+# Create Model for Simple Inventory
 from django.db import models
 
-# Create your models here.
+
+# Create Class to define the model
+class Item(models.Model):
+    name_item = models.CharField(max_length=255)
+    description = models.TextField()
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
