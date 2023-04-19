59d4559b7f9523bc015979a72a3712a2f841d33d
sjtg <chipilej@gmail.com>
2023-04-19 10:51:20 +0200

    removed secret key

--- simple_inventory/settings.py
+++ simple_inventory/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n4^yk)b+#!qzl7fw38x1l@hn5hx#$9#1s4&341zz(5is#u@n14"
-
+SECRET_KEY = "(secret key)"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
