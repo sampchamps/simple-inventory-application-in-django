96ab57887d84fd403c713f4ed977b60c976bf79b
Joshua Chipile <chipilej@gmail.com>
2023-04-20 22:33:11 +0200

    Graphql (#1)
    * added graphql  and schema
    
    * Fixed the results which were not displaying when use gragphql

--- inventoryapp/models.py
+++ inventoryapp/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 # Create Class to define the model
+
+
 class Item(models.Model):
     name_item = models.CharField(max_length=255)
     description = models.TextField()
@@ -10,3 +12,6 @@ class Item(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name_item
--- simple_inventory/settings.py
+++ simple_inventory/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "inventoryapp",
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
