253221e953d5414382aace0810e70fb12b000d7d
sjtg <chipilej@gmail.com>
2023-04-20 11:01:02 +0200

    added graphql  and schema

---
+++ inventoryapp/schema.py
@@ -0,0 +1,21 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from .models import Item
+
+
+class ItemType(DjangoObjectType):
+    class Meta:
+        model = Item
+        fields = ("id", "name_item", "description")
+
+
+class Query(graphene.ObjectType):
+    all_items = graphene.List(ItemType)
+
+    def resolve_all_itemts(root, info):
+        # We can easily optimize query count in the resolve method
+        return Item.objects.all()
+
+
+schema = graphene.Schema(query=Query)
