158bc6af0ee6541a371fe6d57ac48db8f97778cc
Joshua Chipile <chipilej@gmail.com>
2023-04-28 19:32:55 +0200

    Added Category (#2)
    * Created template and view file for inventory application
    
    * created extra template (files)
    
    * created css and js folder in the inventory_templates folder
    
    * Added category model

--- inventoryapp/models.py
+++ inventoryapp/models.py
@@ -3,11 +3,18 @@ from django.db import models
 
 
 # Create Class to define the model
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+    parent = models.ForeignKey("Category", on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.category_name
 
 
 class Item(models.Model):
     name_item = models.CharField(max_length=255)
     description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
--- inventoryapp/urls.py
+++ inventoryapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
 from inventoryapp.schema import schema
+from . import views
 
 urlpatterns = [
     path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
+    path("", views.item_list, name="item_list"),
 ]
