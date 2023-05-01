258665f047f9c8f5fe470cf987f92e8ccb27b767
Joshua Chipile <chipilej@gmail.com>
2023-05-01 22:11:17 +0200

    Created Test for my model (#3)

--- inventoryapp/tests.py
+++ inventoryapp/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from inventoryapp.models import Category, Item
+from datetime import datetime
 
-# Create your tests here.
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        # create some test data
+        Category.objects.create(category_name="Books")
+
+    def test_category_model(self):
+        # retrieve the test data
+        my_model = Category.objects.get(category_name="Books")
+
+        # check that the data was created correctly
+        self.assertEqual(my_model.category_name, "Books")
+
+
+class ItemTestCase(TestCase):
+    def setUp(self):
+        # create some test data
+        category = Category.objects.create(category_name="Books")
+
+        Item.objects.create(
+            name_item="science",
+            description="science books",
+            category=category,
+            quantity=12,
+            price=123,
+        )
+
+    def test_item_model(self):
+        # retrieve the test data
+        category = Category.objects.get(category_name="Books")
+
+        my_model = Item.objects.get(
+            name_item="science",
+            description="science books",
+            quantity=12,
+            price=123,
+            category=category,
+        )
+
+        # check that the data was created correctly
+        self.assertEqual(my_model.name_item, "science")
+        self.assertEqual(my_model.description, "science books")
+        self.assertEqual(my_model.category.category_name, "Books")
+        self.assertEqual(my_model.quantity, 12)
+        self.assertEqual(my_model.price, 123)
