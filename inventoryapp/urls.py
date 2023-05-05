50aeb7965a2a1b2819a0b851fedc9806663d6c51
sjtg <chipilej@gmail.com>
2023-05-05 10:21:31 +0200

    Added base file to the html files

--- inventoryapp/urls.py
+++ inventoryapp/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
-    path("", views.item_list, name="item_list"),
+    # path("", views.item_list, name="item_list"),
+    path("", views.item_home, name="item_home"),
+    # path("", views.item_form, name="item_form"),
+    # path("", views.item_bulk_upload, name="item_bulk_upload"),
 ]
--- inventoryapp/views.py
+++ inventoryapp/views.py
@@ -5,5 +5,19 @@ from .models import Item
 # Create your views here.
 
 
+def item_home(request):
+    homes = Item.objects.all()
+    return render(request, "inventory_templates/item_home.html", {homes: "homes"})
+
+
 def item_list(request):
-    return render(request, "inventory_templates/item_list.html", {})
+    items = Item.objects.all()
+    return render(request, "inventory_templates/item_list.html", {items: "items"})
+
+
+def item_form(request):
+    return render(request, "inventory_template/item_form.html", {})
+
+
+def item_bulk_upload(request):
+    return render(request, "inventory_templates/item_bulk_upload.html", {})
